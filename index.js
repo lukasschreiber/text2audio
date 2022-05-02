063a4d364c7abef407d35629a9ff27b24f06444f
lukasschreiber <info@lukasschreiber.com>
2022-05-02 20:08:06 +0200

    format

--- index.js
+++ index.js
@@ -8,36 +8,35 @@ const context = new AudioContext();
 const master = context.createGain();
 master.connect(context.destination);
 
-document.querySelector('#start').addEventListener('click', async ()=>{
+document.querySelector('#start').addEventListener('click', async () => {
     const SETTINGS = getInputSettings();
-    console.log(SETTINGS);
 
-    for(let letter of Array.from(SETTINGS.text)){
+    for (let letter of Array.from(SETTINGS.text)) {
         // const LETTER = getLetter(letter); // for vertical Text
         const LETTER = transpose(mirror(getLetter(letter))); // for Horizontal Text
 
         // printLetter(LETTER);
-        for(let row = LETTER.length - 1; row >= 0; row--){
+        for (let row = LETTER.length - 1; row >= 0; row--) {
             const TONES = [];
-            for(let i = 0; i < LETTER[row].length; i++){
-                if(LETTER[row][i] === 1){
-                    const LBASE = LETTER[row].length <= LINE_HEIGHT ? SETTINGS.base : SETTINGS.base - (LETTER[row].length - LINE_HEIGHT)*INTERVAL;
+            for (let i = 0; i < LETTER[row].length; i++) {
+                if (LETTER[row][i] === 1) {
+                    const LBASE = LETTER[row].length <= LINE_HEIGHT ? SETTINGS.base : SETTINGS.base - (LETTER[row].length - LINE_HEIGHT) * INTERVAL;
                     TONES.push(LBASE + (INTERVAL * i));
                 }
             }
-    
+
             let j = 0;
-            if(TONES.length > 0){
-                for(let i = 0; i < TONES.length * THICKNESS; i++){
+            if (TONES.length > 0) {
+                for (let i = 0; i < TONES.length * THICKNESS; i++) {
                     const ACTUAL_LENGTH = LENGTH / (TONES.length * THICKNESS);
                     playFrequency(TONES[j], ACTUAL_LENGTH);
                     await wait(ACTUAL_LENGTH);
-                    j = j < (TONES.length - 1) ? j+1 : 0;
+                    j = j < (TONES.length - 1) ? j + 1 : 0;
                 }
-            }else{
+            } else {
                 await wait(LENGTH);
             }
-            
+
         }
 
         await wait(LENGTH);
@@ -45,7 +44,7 @@ document.querySelector('#start').addEventListener('click', async ()=>{
 
 });
 
-document.querySelector('#stop').addEventListener('click', ()=>{
+document.querySelector('#stop').addEventListener('click', () => {
     master.gain.setTargetAtTime(0, context.currentTime, 0.015);
 });
 
@@ -57,43 +56,43 @@ const playFrequency = (frequency, duration) => {
     channel.connect(master);
 
     oscillator.start(0);
-    
-    setTimeout(()=>{channel.gain.setTargetAtTime(0, context.currentTime, SLOPE);}, duration);
-}
 
-const wait = (time) => new Promise(res=>setTimeout(res,time));
+    setTimeout(() => { channel.gain.setTargetAtTime(0, context.currentTime, SLOPE); }, duration);
+};
+
+const wait = (time) => new Promise(res => setTimeout(res, time));
 
 const transpose = (array) => array[0].map((_, colIndex) => array.map(row => row[colIndex]));
-const mirror = (array)  => array.map(row => row.reverse());
+const mirror = (array) => array.map(row => row.reverse());
 
 const printLetter = (letter) => {
-    let out = ""
-    for(let row of letter){
-        out += row.map(c => c === 0 ? " " : "O").join("")+"\n";
+    let out = "";
+    for (let row of letter) {
+        out += row.map(c => c === 0 ? " " : "O").join("") + "\n";
     }
-    
-    if(out !== "") console.log(out);
-}
+
+    if (out !== "") console.log(out);
+};
 
 const getLetter = (letter) => {
     let letters = JSON.parse(JSON.stringify(LETTERS)); // hack to copy object
     // translate letters
-    switch(letter){
+    switch (letter) {
         case " ": letter = "SPACE"; break;
         case "🤖": letter = "ROBOT"; break;
         case "🖤": letter = "FILLED_HEART"; break;
         case "🤍": letter = "STROKED_HEART"; break;
     }
 
-    if(!letters[letter]) letter = "?";
+    if (!letters[letter]) letter = "?";
 
     return letters[letter];
-}
+};
 
 const getInputSettings = () => {
     return {
         text: document.querySelector('#input').value,
         base: parseInt(document.querySelector('#base').value) || 18000
     };
-}
+};
 
